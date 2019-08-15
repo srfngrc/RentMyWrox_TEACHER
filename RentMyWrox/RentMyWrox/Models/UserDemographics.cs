37d82848d66faeafedac7c3ad2f705d8b02270e6
srfngrc <srfngrc@gmail.com>
2019-08-15 18:08:36 -0400

    almost...

---
+++ RentMyWrox/RentMyWrox/Models/UserDemographics.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RentMyWrox.Models
+{
+    public class UserDemographics
+    {
+        public UserDemographics()
+        {
+            Hobbies = new List<Hobby>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+        public DateTime Birthdate { get; set; }
+
+        public string Gender { get; set; }
+
+        public string MaritalStatus { get; set; }
+
+        public DateTime DateMovedIntoArea { get; set; }
+
+        public bool OwnHome { get; set; }
+
+        public int TotalPeopleInHome { get; set; }
+
+        public List<Hobby> Hobbies { get; set; }
+    }
+
+    public class Hobby
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public virtual ICollection<UserDemographics> UserDemographics { get; set; }
+    }
+}
